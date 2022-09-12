89d401a98480c26bbda3406682bec34f79d1c4c9
Dasian <davidrespiritu99@gmail.com>
2022-09-13 07:03:38 +0800

    Fixed role list embedded message limit bug

--- embedder.py
+++ embedder.py
@@ -224,10 +224,10 @@ def second_arg(msg):
 # returns an embedded message containing a list of accessible 
 # channels with their respective servers
 # c is an ordered list of (server, channel)
+MAX_FIELD_LEN = 1024
 def channels(list):
   print("Generating accessible channels")
   e = discord.Embed(title="Accessible Channels", description="List of channels that I'm able to publish to", color = 0x55FDF9)
-  MAX_FIELD_LEN = 1024
   e.set_footer(text="run corn?publish [channel_id] to publish the current embedded message to that channel")
   id = 0
   while id < len(list):
@@ -262,7 +262,14 @@ def role_list(list):
     server = list[id][0]
     while id < len(list) and list[id][0] == server:
       role = list[id][1]
-      roles += 'Role: ' + role.name + '\n'
+      new_role = 'Role: ' + role.name + '\n'
+
+      if len(roles) + len(new_role) < MAX_FIELD_LEN:
+        roles += new_role
+      else:
+        e.add_field(name="Server: "+str(server), value=roles, inline=False)
+        roles = new_role
+        
       id += 1
     # create a field where the title is the server name 
     # and the contents are the accessible channels with their id
