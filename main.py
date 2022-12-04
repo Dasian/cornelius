83725266b02ac20b165fa51a4c0801fc92b4cd7b
Dasian <davidrespiritu99@gmail.com>
2022-12-04 01:59:59 -0500

    Imitation cmd; moving to linux

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 import os
 from random import randint
-import discord
+import discord 
 import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -15,6 +15,14 @@ import embedder
 import admin_cmds
 import server_cmds
 
+# custom voice
+import aiohttp
+import tempfile
+import subprocess
+import asyncio
+from io import BytesIO
+import json
+import time
 
 # global vars
 cmd_start = '!' # change this at will bro
@@ -30,15 +38,20 @@ ping_embed = None
 publish_channel = None
 # bot obj for bot commands (all intents enabled but could specify if you want)
 # extended off of client
+print(discord.__version__)
 bot = commands.Bot(command_prefix=cmd_start, intents=discord.Intents.all())
 
 # guess what this function is
 def main():
   # retrieve secrets
   global admins
+  global API_KEY, API_ROOT, API_SECRET
   load_dotenv()
   TOKEN = os.environ['TOKEN']
   PERMS = os.environ['PERMS']
+  API_ROOT = 'https://api.uberduck.ai'
+  API_KEY = os.environ['UBERDUCK_API_KEY']
+  API_SECRET = os.environ['UBERDUCK_API_SECRET']
   NUM_ADMINS = int(os.environ['NUM_ADMINS'])
   for i in range(0, NUM_ADMINS):
     s = "ADMIN" + str(i)
@@ -194,14 +207,83 @@ async def revive(ctx):
   revival_msg = '<@&' + str(rid) + '>'
   await ctx.send(revival_msg)
 
-@bot.event
-async def on_command_error(ctx, error):
+@revive.error
+async def revive_error(ctx, error):
   degrading_msgs = ['Touch some grass', 'Get some bitches']
   if isinstance(error, commands.CommandOnCooldown):
-    cooldown_msg = 'This cmd is on cooldwon for ' + str(int(error.retry_after)) + ' more seconds'
+    cooldown_msg = 'This cmd is on cooldown for ' + str(int(error.retry_after)) + ' more seconds'
     r = randint(0, len(degrading_msgs))
     cooldown_msg += '\n' + degrading_msgs[r]
     await ctx.send(cooldown_msg)
+
+# send a custom tts (text to speech) message with a custom voice
+@bot.command()
+async def imitate(ctx, voice, *, message):
+  global voice_client
+  voice_client = ctx.author.voice
+  if voice_client:
+    voice_client = await voice_client.channel.connect()
+    
+    # uberduck copied code
+    # await ctx.response.defer(ephemeral=True, with_message=True)
+    audio_data = await query_uberduck(message, voice)
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as wav_f, tempfile.NamedTemporaryFile(suffix=".opus") as opus_f:
+      wav_f.write(audio_data.getvalue())
+      wav_f.flush()
+      await ctx.send("writing complete")
+      subprocess.check_call(["ffmpeg", "-y", "-i", wav_f.name, opus_f.name])
+      await ctx.send('ffmpeg done')
+      source = discord.FFmpegOpusAudio(opus_f.name) # ffmpeg executable needs to be in path env variable to work
+      voice_client.play(source, after=None)
+      while voice_client.is_playing():
+        await asyncio.sleep(0.5)
+        
+    await voice_client.disconnect()
+  else:
+    await ctx.send("You need to be connected to a voice channel")
+  return
+
+async def query_uberduck(text, voice="zwf"):
+  # copied from uberduck blog
+  max_time = 60
+  async with aiohttp.ClientSession() as session:
+    url = f"{API_ROOT}/speak"
+    data = json.dumps(
+      {
+        "speech": text,
+        "voice": voice,
+      }
+    )
+
+    start = time.time()
+    async with session.post(
+      url,
+      data=data,
+      auth=aiohttp.BasicAuth(API_KEY, API_SECRET),
+    ) as r:
+      if r.status != 200:
+        raise Exception("Error synthesizing speech", await r.json())
+      uuid = (await r.json())["uuid"]
+    while True:
+      if time.time() - start > max_time:
+        raise Exception("Request timed out!")
+      await asyncio.sleep(1)
+      status_url = f"{API_ROOT}/speak-status"
+      async with session.get(status_url, params={"uuid": uuid}) as r:
+        if r.status != 200:
+          continue
+        response = await r.json()
+        if response["path"]:
+          async with session.get(response["path"]) as r:
+            return BytesIO(await r.read())
+  return
+
+@imitate.error
+async def imitate_error(ctx, error):
+  # TODO make this more descriptive
+  await ctx.send(str(error))
+  if type(voice_client) == discord.voice_client.VoiceClient:
+    await voice_client.disconnect()
 
 if __name__ == '__main__':
   main()
